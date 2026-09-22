4ca35fbcde55ec3082839f54689b7c88d22ac2d3
Wiktor Lutowski <wiktor.luto@gmail.com>
2026-09-22 18:45:10 +0200

    Add models

---
+++ src/api/PlaylistArchivizer.Domain/Models/Playlist.cs
@@ -0,0 +1,20 @@
+﻿namespace PlaylistArchivizer.Domain.Models
+{
+    public class Playlist
+    {
+        public string Id { get; init; }
+        public string Name { get; init; }
+        public string SnapshotId { get; init; }
+        public List<Track> Tracks { get; init; } = [];
+
+        public Playlist(string id, string name, string snapshotId, IEnumerable<Track>? tracks = null)
+        {
+            Id = id;
+            Name = name;
+            SnapshotId = snapshotId;
+
+            if (tracks != null)
+                Tracks.AddRange(tracks);
+        }
+    }
+}
---
+++ src/api/PlaylistArchivizer.Domain/Models/Track.cs
@@ -0,0 +1,22 @@
+﻿namespace PlaylistArchivizer.Domain.Models
+{
+    public class Track
+    {
+        public string Id { get; init; }
+        public string Name { get; init; }
+        public DateTime? AddedAt { get; init; }
+        public string ImageUrl { get; init; }
+        public IReadOnlyCollection<string> Artists => _artists.AsReadOnly();
+
+        private readonly List<string> _artists = [];
+
+        public Track(string id, string name, DateTime? addedAt, IEnumerable<string> artists, string imageUrl)
+        {
+            Id = id;
+            Name = name;
+            AddedAt = addedAt;
+            ImageUrl = imageUrl;
+            _artists.AddRange(artists ?? []);
+        }
+    }
+}
